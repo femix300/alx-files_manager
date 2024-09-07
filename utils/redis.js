c8203b3c474723cc8e38d1075188d08cbe61f2d7
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2024-09-07 04:33:16 +0100

    added more files

--- utils/redis.js
+++ utils/redis.js
@@ -4,7 +4,7 @@ const { createClient } = require('redis');
 const { promisify } = require('util');
 
 class RedisClient {
-  constructor() {
+  constructor () {
     this.client = createClient();
     this.client.on('error', (err) => console.log(err));
     this.connected = false;
@@ -13,22 +13,22 @@ class RedisClient {
     });
   }
 
-  isAlive() {
+  isAlive () {
     return this.connected;
   }
 
-  async get(key) {
+  async get (key) {
     const getAsync = promisify(this.client.get).bind(this.client);
     const val = await getAsync(key);
     return val;
   }
 
-  async set(key, val, dur) {
+  async set (key, val, dur) {
     const setAsync = promisify(this.client.set).bind(this.client);
     await setAsync(key, val, 'EX', dur);
   }
 
-  async del(key) {
+  async del (key) {
     const delAsync = promisify(this.client.del).bind(this.client);
     await delAsync(key);
   }
